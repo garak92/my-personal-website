4397162bc2094696f4ff884e993bafee5cabf435
Elim Garak <garak92@protonmail.com>
2022-09-15 23:12:08 -0400

    Updated linkedin link

--- frontend/src/components/common/Footer.js
+++ frontend/src/components/common/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
     <>
       <div className='footer'>
         <SocialIcon className='socials' url='https://github.com/garak92' target='_blank' rel='noopener noreferrer' />
-        <SocialIcon className='socials' url='https://www.linkedin.com/in/fabrizio-p-53a117195/' target='_blank' rel='noopener noreferrer' />
+        <SocialIcon className='socials' url='https://www.linkedin.com/in/fabrizio-m-p-53a117195/' target='_blank' rel='noopener noreferrer' />
       </div>
     </>
   )
